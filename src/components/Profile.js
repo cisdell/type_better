c170a051eb7e9bf82ca342e1a307597bf514dfc8
Andrew Cho <cisdell@gmail.com>
2023-08-10 16:45:19 -0400

    remodling the side component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,8 @@
-import React from 'react'
-
+import userData from "../userData.json";
+const userInfo = userData.uuid1;
 export default function Profile() {
-  return (
-    <div className="profile">USER NAME</div>
-  )
+  console.log(userInfo);
+  const { user, total_time, highest_clear_count, times_played, last_time } =
+    userInfo;
+  return <div className="profile">HELLO</div>;
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 
 //components.
 import Profile from "./Profile";
@@ -9,14 +9,13 @@ import Brick from "./Brick.js";
 // import "./Sidebar.css";
 
 export default function Sidebar() {
-  const [life, setLife] = useState(5)
+  const [life, setLife] = useState(5);
 
   return (
     <div className="sidebar">
       WELCOME!
-      <Profile />
       <div className="sidebar-content">
-
+        <Profile />
       </div>
     </div>
   );
