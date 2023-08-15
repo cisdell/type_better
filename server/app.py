f43d0d66b8fc9ccf67f844129ac93b75ba7a30b3
Andrew Cho <cisdell@gmail.com>
2023-08-15 19:23:51 -0400

    testing flask implementation

--- server/app.py
+++ server/app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+import psychopg2
+from dotenv import load_dotenv
+import os
+# from flask_sqlalchemy import SQLAlchemy
+CREATE_ROOMS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
+)
 
+
+
+load_dotenv()
+
+db_uri = os.getenv('DB_URI')
+connection = psycopg2.connect(db_uri)
 app = Flask(__name__)
-@app.get("/")
-def home():
-    return "TEST HOME"
+
+@app.route("/api/room")
+def create_room():
+    data = request.get_json()
+    name = data["name"]
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_ROOMS_TABLE)
+
+
---
+++ server/seed.sql
@@ -0,0 +1,4 @@
+-- Create a new database called 'DatabaseName'
+CREATE DATABASE GAME_STAT if not exist;
+/c GAME_STAT;
+
