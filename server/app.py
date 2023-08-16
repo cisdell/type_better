7356640f8a423c3a2cc2dfc4aa382f37a3798e5a
Andrew Cho <cisdell@gmail.com>
2023-08-15 21:37:21 -0400

    sql queries

--- server/app.py
+++ server/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request
-import psychopg2
+import psycopg2
 from dotenv import load_dotenv
+from datetime import datetime, timezone
 import os
 # from flask_sqlalchemy import SQLAlchemy
-CREATE_ROOMS_TABLE = (
-    "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
-)
 
+#Queries
+# CREATE_ROOMS_TABLE = "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
+# INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
 
 
 load_dotenv()
@@ -15,12 +16,26 @@ db_uri = os.getenv('DB_URI')
 connection = psycopg2.connect(db_uri)
 app = Flask(__name__)
 
-@app.route("/api/room")
-def create_room():
+# @app.post("/api/room")
+# def create_room():
+#     data = request.get_json()
+#     name = data["name"]
+#     with connection:
+#         with connection.cursor() as cursor:
+#             cursor.execute(CREATE_ROOMS_TABLE)
+#             cursor.execute(INSERT_ROOM_RETURN_ID, (name,))
+#             room_id = cursor.fetchone()[0]
+#     return {"id": room_id, "message": f"Room {name} created."}, 201
+
+
+@app.post("/updateStat")
+def update_stat():
     data = request.get_json()
-    name = data["name"]
+    #received data from the client
+
+
+@app.route("/getStat")
+def get_stat():
     with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(CREATE_ROOMS_TABLE)
 
-
+    pass
--- server/seed.sql
+++ server/seed.sql
@@ -1,4 +1,20 @@
 -- Create a new database called 'DatabaseName'
-CREATE DATABASE GAME_STAT if not exist;
+CREATE DATABASE GAME_STAT if not EXISTS;
+
 /c GAME_STAT;
 
+CREATE TABLE IF NOT EXISTS USERS (
+  email VARCHAR (50) NOT NULL PRIMARY KEY,
+  last_login TIMESTAMP NOT NULL,
+  total_time INT NOT NULL,
+  highest_clear INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS USERS_STAT (
+  email VARCHAR (50) NOT NULL,
+  usage_time TIMESTAMP NOT NULL,
+  time_spent int,
+  clear_count int,
+  FOREIGN KEY (email)
+    REFERENCES USERS (email)
+);
