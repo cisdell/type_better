c5598124a0e990e1e5925d074bfc0dc36302b945
Andrew Cho <cisdell@gmail.com>
2023-09-15 18:53:40 -0400

    footer component coming along.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <div className="footer">Footer ABOUT THE USER</div>;
+  return (
+    <div className="footer">
+      <a href="">LinkedIn</a>
+      <a href="">Contact</a>
+      <a href="">GitHub</a>
+    </div>
+  );
 }
