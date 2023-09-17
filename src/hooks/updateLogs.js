2a253b462e753fc3fe548c3dd706981adce8eecb
Andrew Cho <cisdell@gmail.com>
2023-09-17 08:39:19 -0400

    updated aws backend

--- src/hooks/updateLogs.js
+++ src/hooks/updateLogs.js
@@ -1,12 +1,11 @@
-
-import axios from 'axios';
-const localhost = 'http://127.0.0.1:5000';
-const aws =  'http://ec2-54-88-203-55.compute-1.amazonaws.com:5000'
+import axios from "axios";
+const localhost = "http://127.0.0.1:5000";
+const aws = "http://ec2-18-218-129-28.us-east-2.compute.amazonaws.com:5000";
 
 const instance = axios.create({
   baseURL: aws, // Your backend API URL
   timeout: 1000,
-  headers: {'X-Custom-Header': 'foobar'}
+  headers: { "X-Custom-Header": "foobar" },
 });
 
 export default instance;
